b15a6a1493de4272de2ea15bdd2abf89c1114a71
simoneciresa <104499310+simoneciresa@users.noreply.github.com>
2023-07-11 12:22:56 +0200

    Backup
    Performed before changing the way data are collected

--- SampleListener.cpp
+++ SampleListener.cpp
@@ -41,36 +41,6 @@ void SampleListener::onDisconnect(const Controller& controller) {
   cout << "Disconnected" << endl;
 }
 
-void SampleListener::onExit(const Controller& controller) {
-  cout << "Exited" << endl;
-}
-
-void SampleListener::onFocusGained(const Controller& controller) {
-  cout << "Focus Gained" << endl;
-}
-
-void SampleListener::onFocusLost(const Controller& controller) {
-  cout << "Focus Lost" << endl;
-}
-
-void SampleListener::onDeviceChange(const Controller& controller) {
-  cout << "Device Changed" << endl;
-  const DeviceList devices = controller.devices();
-
-  for (int i = 0; i < devices.count(); ++i) {
-    cout << "id: " << devices[i].toString() << endl;
-    cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << endl;
-  }
-}
-
-void SampleListener::onServiceConnect(const Controller& controller) {
-  cout << "Service Connected" << endl;
-}
-
-void SampleListener::onServiceDisconnect(const Controller& controller) {
-  cout << "Service Disconnected" << endl;
-}
-
 void SampleListener::onFrame(const Controller& controller) {
   
   // Get the most recent frame and report some basic information
